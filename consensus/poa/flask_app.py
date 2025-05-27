236cae0c08da2b3b68b94b92ac8650283530ee05
jefin <techy.jefinjoji@gmail.com>
2025-05-27 13:13:08 +0530

    Add basic proof of authority blockchain structure; known bugs remain

---
+++ consensus/poa/flask_app.py
@@ -0,0 +1,51 @@
+from flask import Flask, Response
+from collections import OrderedDict
+import json
+from blochain_structures import Chain, txs_to_json_digestable_form
+
+
+def create_flask_app(peer):
+    app = Flask(__name__)
+
+    @app.route("/status", methods=["GET"])
+    def get_status():
+        chain_list = []
+        for block in Chain.instance.chain:
+            chain_list.append({
+                "id": block.id,
+                "prevHash": block.prevHash,
+                "transactions": txs_to_json_digestable_form(block.transactions),
+                "ts": block.ts,
+                "hash": block.hash
+            })
+        outbound_peers_list = []
+        for outbound_peer in peer.outbound_peers:
+            outbound_peers_list.append({
+                "addr": outbound_peer[0],
+                "port": outbound_peer[1]
+            })
+        return Response(
+            json.dumps(OrderedDict([
+                ("name", peer.name),
+                ("host", peer.host),
+                ("port", peer.port),
+                ("is_miner", peer.miner),
+                ("admin_id", peer.admin_id),
+                ("node_id", peer.node_id),
+                ("miners", list(peer.miners)),
+                ("name to node id dict", peer.name_to_node_id_dict),
+                ("node_id_to_name_dict", peer.node_id_to_name_dict),
+                ("known_peers", list(map(lambda x: peer.known_peers[x][0]+":"+x[0]+":"+str(x[1])+":"+peer.known_peers[x][1], peer.known_peers.keys()))),
+                ("outbound_peers", outbound_peers_list),
+                ("client_connections", list(ws.remote_address[1] for ws in peer.client_connections)),
+                ("server_connections", list(ws.remote_address[1] for ws in peer.server_connections)),
+                ("mempool", txs_to_json_digestable_form(list(peer.mem_pool))),
+                ("chain", chain_list)
+            ])),
+            mimetype='application/json'
+        )
+    
+    return app
+    
+def run_flask_app(app, port):
+    app.run(host="0.0.0.0", port=port + 20)

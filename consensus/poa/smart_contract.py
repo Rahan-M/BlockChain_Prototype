d09f04a087942dcd047ab59f80440bd2b51307ee
jefin <techy.jefinjoji@gmail.com>
2025-06-23 07:34:01 +0530

    Add basic smart contract wrapper in proof of authority

---
+++ consensus/poa/smart_contract.py
@@ -0,0 +1,33 @@
+from RestrictedPython import compile_restricted
+from RestrictedPython.Eval import default_guarded_getiter
+from RestrictedPython.Guards import safe_builtins
+from RestrictedPython.PrintCollector import PrintCollector
+
+def _getitem_(obj, index):
+    return obj[index]
+
+def _write_(obj):
+    return obj
+
+class ContractEnvironment:
+    def __init__(self, code: str):
+        self.code = code
+        self.globals = {
+            '__builtins__': safe_builtins,
+            '_getiter_': default_guarded_getiter,
+            '_getitem_': _getitem_,
+            '_write_': _write_,
+            '_print_': PrintCollector,
+        }
+        self.locals = {}
+        self._compile()
+
+    def _compile(self):
+        self.compiled = compile_restricted(self.code, filename='<contract>', mode='exec')
+        exec(self.compiled, self.globals, self.locals)
+
+    def run_contract(self, func_name: str, *args):
+        func = self.locals.get(func_name)
+        if not func:
+            raise Exception(f"Function '{func_name}' not found in contract.")
+        return func(*args)

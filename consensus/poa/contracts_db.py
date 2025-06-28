e76ececafb7104cd31e9dfba85b4d00c5fda9caf
jefin <techy.jefinjoji@gmail.com>
2025-06-28 07:55:21 +0530

    Remove unwanted functions from proof of authority SmartContractDatabase class to store contract state on chain

--- consensus/poa/contracts_db.py
+++ consensus/poa/contracts_db.py
@@ -1,17 +1,9 @@
 class SmartContractDatabase:
     def __init__(self):
         self.contracts = {}
-        self.contract_states = {}
     
     def store_contract(self, contract_id, code):
         self.contracts[contract_id] = code
-        self.contract_states[contract_id] = {}
 
     def get_contract(self, contract_id):
         return self.contracts.get(contract_id)
-    
-    def get_contract_state(self, contract_id):
-        return self.contract_states.get(contract_id, {})
-    
-    def update_contract_state(self, contract_id, new_state):
-        self.contract_states[contract_id] = new_state

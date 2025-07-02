cfa7659c8694f880d75d63a80405af6b556d2480
Rahan-M <rahan01729@gmail.com>
2025-07-02 07:15:44 +0530

    Started work on frontend

---
+++ webApp/pow/frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import Navbar from "./Components/Navbar"
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+    <div className="bg-secondary h-[100vh] w-full">
+      Hey
+    </div>
+    </>
+  )
+}
+
+export default App

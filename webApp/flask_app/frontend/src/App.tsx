bb3bc968cc1a7743c83e29138fbd8b808f31fd37
Rahan-M <rahan01729@gmail.com>
2025-07-05 12:06:25 +0530

    feat: add navbar, test: create keys

--- webApp/flask_app/frontend/src/App.tsx
+++ webApp/flask_app/frontend/src/App.tsx
@@ -1,34 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import Navbar from "./Components/Navbar";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [sk, setSk]=useState("");
+  const [vk, setVk]=useState("");
+  const [loggedIn, setLoggedIn]=useState(false);
+
+  const getKeys=async ()=>{
+    try{
+      const res=await axios.get("/api/chain/create_keys");
+      setSk(res.data.sk);
+      setVk(res.data.vk);
+      setLoggedIn(true);
+    }
+    catch{
+
+    }
+  }
+
+  const getKeysButton=()=>{
+    return(
+      <button className="account_tab bg-primary text-white p-5 rounded-2xl cursor-pointer" onClick={getKeys}>
+        Create Keys
+      </button>
+    )
+  }
+
+  const displayKeys=()=>{
+    return(
+      <div className="keys text-white w-[80vw] bg-primary p-10 rounded-2xl">
+        <div className="vk">
+          Verifying Key = {vk}
+        </div>
+        <br/><br/><br/>
+        <div className="sk">
+          Secret Key = {sk}
+        </div>
+      </div>
+    )
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Navbar/>
+      <div className="bg-secondary h-[90vh] w-full flex justify-center items-center">
+        {!loggedIn && getKeysButton()}
+        {loggedIn && displayKeys()}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      </>
   )
 }
 

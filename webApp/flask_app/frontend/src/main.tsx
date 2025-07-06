c0e6d93bf482b408c5e5e7f6cd999380f560e374
Rahan-M <rahan01729@gmail.com>
2025-07-06 09:52:22 +0530

    feat: created router in main

--- webApp/flask_app/frontend/src/main.tsx
+++ webApp/flask_app/frontend/src/main.tsx
@@ -3,11 +3,31 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import {AuthProvider} from './contexts/AuthContext.tsx'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+
+const NotFound = () => (
+  <div className="min-h-screen bg-black flex items-center justify-center">
+      <span className="text-white text-center text-6xl md:text-8xl font-bold lg:text-nowrap -translate-y-24">
+          404 - Page Not Found
+      </span>
+  </div>
+);
+
+const router = createBrowserRouter([
+  {
+      path: "/",
+      // element: <RootLayout />,
+      children: [
+          { index: true, element: <App /> },
+          { path: "*", element: <NotFound /> }
+      ],
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AuthProvider>
-      <App />
+      <RouterProvider router={router} />
     </AuthProvider>
   </StrictMode>,
 )
